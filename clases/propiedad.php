66ccafb544e5a121c1004cd594f1c796fc2fbe49
Alejandro Balladares <ale_12_river@hotmail.com>
2026-08-27 15:26:14 -0300

    Agrego query dinamico

--- clases/propiedad.php
+++ clases/propiedad.php
@@ -6,7 +6,7 @@ class Propiedad {
 
 //base de datos
     protected static $db;
-    protected static $columnasDB = ['id', 'titulo','precio','imagen','descripcion','habitaciones','wc','estacionamiento','creado','vendedorID'];
+    protected static $columnasDB = ['id', 'titulo','precio','imagen','descripcion','habitaciones','wc','estacionamiento','creado','vendedorId'];
     public $id;
     public $titulo;
     public $precio;
@@ -38,11 +38,16 @@ class Propiedad {
         //sanitizar datos
         $atributos = $this->sanitizarDatos();
 
-        $query = "INSERT INTO propiedades (titulo, precio, imagen, descripcion, habitaciones, wc, estacionamiento, creado, vendedores_id) VALUES ('$this->titulo', 
-        '$this->precio', '$this->imagen', '$this->descripcion', '$this->habitaciones', '$this->wc', '$this->estacionamiento', '$this->creado', '$this->vendedorId')";
+        //$string = join(', ', array_keys($atributos));
 
+        $query = "INSERT INTO propiedades (";
+        $query .= join(', ', array_keys($atributos));
+        $query .= " ) VALUES (' ";
+        $query .= join("', '", array_values($atributos));
+        $query .= " ') ";
+        
         $resultado = self::$db->query($query);
-
+        debuguear($resultado);
     }
 
     //definir conexion a la db

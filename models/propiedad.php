28f3c7b22e863b788c19196868ea0306de7870db
Alejandro Balladares <ale_12_river@hotmail.com>
2026-09-04 15:43:14 -0300

    Agrego los modelos

---
+++ models/propiedad.php
@@ -0,0 +1,62 @@
+<?php 
+
+namespace Model;
+
+use Model\ActiveRecord as ActiveRecord;
+
+class Propiedad extends ActiveRecord{
+    protected static $tabla = 'propiedades';
+    protected static $columnasDB = ['id', 'titulo','precio','imagen','descripcion','habitaciones','wc','estacionamiento','creado','vendedorId'];
+    
+    public $id;
+    public $titulo;
+    public $precio;
+    public $imagen;
+    public $descripcion;
+    public $habitaciones;
+    public $wc;
+    public $estacionamiento;
+    public $creado;
+    public $vendedorId;
+
+    public function __construct($args = [])
+    {
+        $this->id = $args['id'] ?? '';
+        $this->titulo = $args['titulo'] ?? '';
+        $this->precio = $args['precio'] ?? '';
+        $this->imagen = $args['imagen'] ?? '';
+        $this->descripcion = $args['descripcion'] ?? '';
+        $this->habitaciones = $args['habitaciones'] ?? '';
+        $this->wc = $args['wc'] ?? '';
+        $this->estacionamiento = $args['estacionamiento'] ?? '';
+        $this->creado = date('Y/m/d');
+        $this->vendedorId = $args['vendedorId'] ?? '';
+    
+    }
+
+    public function validar(){
+         if(!$this->titulo){
+            self::$errores[] = "Debe agregar un titulo";
+        }
+        if(!$this->precio){
+            self::$errores[] = "Debe agregar un precio";
+        }
+        if(strlen($this->descripcion) <= 10){
+            self::$errores[] = "La descripcion debe ser obligatoria y tener más de 10 caracteres";
+        }
+        if(!$this->wc){
+            self::$errores[] = "Debe agregar unos wc";
+        }
+        if(!$this->estacionamiento){
+            self::$errores[] = "Debe agregar un estacionamiento";
+        }
+        if(!$this->vendedorId){
+            self::$errores[] = "Debe agregar un vendedor";
+        }
+
+        if(!$this->imagen){
+            self::$errores[] = "La imagen es obligatoria";
+        }
+        return self::$errores;
+    }
+}

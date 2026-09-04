8fa5474a3e63cb53dfe486d7bb9e5e7400a8656e
Alejandro Balladares <ale_12_river@hotmail.com>
2026-09-04 15:42:56 -0300

    Agrego la carpeta de controladores

---
+++ controllers/PropiedadController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Controller;
+use MVC\Router;
+use Model\Propiedad;
+
+
+class PropiedadController{
+    public static function index(Router $router){
+        $propiedades = Propiedad::all();
+        $resultado = null;
+        $router->render("propiedades/admin", ['propiedades'=>$propiedades, 'resultado'=>$resultado]);
+
+    }
+    public static function crear(){
+        echo "crear propiedad";
+    }
+    public static function actualizar(){
+        echo "actualizar";
+    }
+}

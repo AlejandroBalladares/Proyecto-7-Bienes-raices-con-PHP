f2699dd06c15d8796ceb319fc776d9fa668cc806
Alejandro Balladares <ale_12_river@hotmail.com>
2026-09-05 15:24:03 -0300

    Agrego los vendedores a la vista principal

--- views/propiedades/admin.php
+++ views/propiedades/admin.php
@@ -9,8 +9,8 @@
             <?php }
             } ?>
             
-        <a href="/propiedades/crear.php" class="boton boton-verde"> Nueva propiedad</a>
-        <a href="/vendedores/crear.php" class="boton-amarillo"> Nuevo vendedor</a>
+        <a href="/propiedades/crear" class="boton boton-verde"> Nueva propiedad</a>
+        <a href="/vendedores/crear" class="boton-amarillo"> Nuevo vendedor</a>
 
         <h2>Propiedades</h2>
         <table class="propiedades">
@@ -32,12 +32,42 @@
                     <td><img src="/imagenes/<?php echo $propiedad->imagen; ?> " class="imagen-tabla"></td>
                     <td> $<?php echo $propiedad->precio; ?> </td>
                     <td>
-                        <form class="w-100" method="POST">
+                        <form class="w-100" method="POST" action="/propiedades/eliminar">
                             <input type="hidden" name="id" value="<?php echo $propiedad->id; ?>">
                             <input type="hidden" name="tipo" value="propiedad">
                             <input type="submit" class="boton-rojo-block" value="Eliminar">
                         </form>
                         <a href="/propiedades/actualizar?id=<?php echo $propiedad->id; ?>" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+
+        <h2>Venderores</h2>
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Telefono</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody> <!-- Mostrar los resultados-->
+
+                <?php foreach($vendedores as $vendedor): ?>
+                <tr>
+                    <td> <?php echo $vendedor->id; ?> </td>
+                    <td> <?php echo $vendedor->nombre . " " . $vendedor->apellido; ?> </td>
+                    <td> <?php echo $vendedor->telefono; ?> </td>
+                    <td>
+                        <form class="w-100" method="POST" action="/vendedores/eliminar">
+                            <input type="hidden" name="id" value="<?php echo $vendedor->id; ?>">
+                            <input type="hidden" name="tipo" value="vendedor">
+                            <input type="submit" class="boton-rojo-block" value="Eliminar">
+                        </form>
+                        <a href="/vendedores/actualizar?id=<?php echo $vendedor->id; ?>" class="boton-amarillo-block">Actualizar</a>
                     </td>
                 </tr>
                 <?php endforeach; ?>
